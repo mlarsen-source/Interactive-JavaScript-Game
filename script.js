23dde171ef0af0a4303df948f89cfc7f248ac232
rjfredr <fredrickalexander1727@gmail.com>
2025-09-23 14:21:50 -0700

    cat health and logic

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ Filename: script.js
 File Description: JavaScript Interactive JavaScript-Game
 
 */
+let health = 100;
+let healthbar = document.getElementById("healthbar");
+
+document.getElementById("bosscat").onclick = function() {
+    if (health>0) {
+        health-=Math.floor(Math.random()*5) +1;
+        healthbar.innerHTML = health;
+    }
+    else {
+        alert("You Win!")
+    }
+}
+
+// let dialouge = document.getElementById("dialouge");
+// function message() {
+//     dialouge.innerHTML = "You suck!"
+// }
+// setTimeout(message,5000);
